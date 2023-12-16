e64faeed77a90ad914eef12037f30614321d5247
Chiqueto <lfchiqueto@gmail.com>
2023-12-16 13:31:45 -0300

    Inserção de novas tela e opções

--- script.js
+++ script.js
@@ -6,14 +6,14 @@ function toggleMode(){
 
     const img = document.querySelector("#profile img")
 
-
+/*
     if(html.classList.contains('light')) {
         img.setAttribute('src','./assets/avatar-light.png') & img.setAttribute('alt','hinata sério iluminado')
     } else {
         img.setAttribute('src','./assets/avatar-dark.png') & img.setAttribute('alt','hinata baddas full dark')
         
     }
-    
+    */
     
 }
 
